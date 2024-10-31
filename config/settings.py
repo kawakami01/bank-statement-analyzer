a583395358fdb0aa40f0b8256a3dd2fe185c1ff6
Miguel Kawakami <miguel.kawakami@noleak.io>
2024-10-31 07:41:17 -0300

    add temporary config file

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+# TODO: Criar um carregamento de configurações com YAML e .env
+
+SUPPORTED_BANKS = {
+    'xp': {
+        'credit_card': 'xp_credit_card_processor',
+        'checking_account': 'xp_checking_account_processor'
+    }
+}
